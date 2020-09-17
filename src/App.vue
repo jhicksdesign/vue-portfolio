a9908ea0f4f14f7eadd11216387ec698bb1dadb1
Jordan Hicks <jhicks@jhicksdesign.com>
2020-09-17 00:16:54 -0600

    Working transitions.

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,21 @@
     <v-btn icon class="hidden-sm-and-up" @click="$refs.drawer.drawer = !$refs.drawer.drawer"><v-icon>mdi-menu</v-icon></v-btn>
     <Nav ref="drawer"/>
     <v-main>
-      <router-view></router-view>
+      <transition
+              mode="out-in"
+              enter-active-class="animate__animated animate__fadeInUpBig"
+              leave-active-class="animate__animated animate__lightSpeedOutLeft"
+              appear
+      >
+        <router-view/>
+      </transition>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import Nav from './components/Nav'
+import 'animate.css'
 
 export default {
   name: 'App',
