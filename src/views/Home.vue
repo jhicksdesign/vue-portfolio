eb127e7653a3af7ca4e0972c9ad9dd9fad97296c
Jordan Hicks <jhicks@jhicksdesign.com>
2020-10-28 20:51:09 -0600

    Added images, edit image location.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,40 +18,40 @@ export default {
         {
           title: "ConcertCrawl",
           content: "Website designed as capstone project for Deep Dive Coding full stack bootcamp, was a culmination of all the technologies learned during the bootcamp. Developed in a group of three, coordinated using GIT as a version control software and Asana to facilitate development in an agile fashion. Front end was designed using React and Redux. Backend was designed using node, express, and MySQL for the database. ",
-          image: "../assets/2ts88b2.png",
+          image: "/static/concert-crawl/FooSearch.png",
           images: [
-            {src: "http://www.fillmurray.com/350/450"},
-            {src: "http://www.fillmurray.com/300/500"},
-            {src: "http://www.fillmurray.com/250/550"}
+            {src: "/static/concert-crawl/FooSearch.png"},
+            {src: "/static/concert-crawl/SearchExample.png"},
+            {src: "/static/concert-crawl/SaveConcerts.png"}
           ]
         },
         {
           title: "Suzy S. Photography",
           content: "Portfolio project that I created as a solo project to help a beginner photographer get more exposure. Designed using HTML/CSS and some Javascript, used bootstrap for a css framework and express was used to set up the mail form functionality.",
-          image: "http://www.fillmurray.com/400/400",
+          image: "/static/suzy-s-photography/MainImage.jpeg",
           images: [
-            {src: "http://www.fillmurray.com/450/450"},
-            {src: "http://www.fillmurray.com/500/500"},
-            {src: "http://www.fillmurray.com/550/550"}
+            {src: "/static/suzy-s-photography/MainImage.jpeg"},
+            {src: "/static/suzy-s-photography/PortfolioGrid.jpg"},
+            {src: "/static/suzy-s-photography/Contact.png"}
           ]
         },
         {
           title: "Missouri Kids",
           content: "Capstone website for my web design associates, designed using WordPress with a team of eight other students. Website was a complete redesign for the non profit organization Missouri Kids, my main role on this project was designing the hom page and overall look and feel of the website.",
-          image: "http://www.fillmurray.com/400/400",
+          image: "/static/missouri-kids/MainImage.png",
           images: [
-            {src: "http://www.fillmurray.com/450/450"},
-            {src: "http://www.fillmurray.com/500/500"},
-            {src: "http://www.fillmurray.com/550/550"}
+            {src: "/static/missouri-kids/MainImage.png"},
+            {src: "/static/missouri-kids/MissouriStatement.png"},
+            {src: "/static/missouri-kids/Sponsors.png"}
           ]
         }, {
           title: "JHicks Design",
           content: "First portfolio project I designed for myself, design using HTML/CSS with some Javascript, used bootstrap for css framework. Was a project to tie together my creative skills with my web design skills to sell myself as a developer.",
-          image: "http://www.fillmurray.com/400/400",
+          image: "/static/jhicks-design/Banner.jpg",
           images: [
-            {src: "http://www.fillmurray.com/450/450"},
-            {src: "http://www.fillmurray.com/500/500"},
-            {src: "http://www.fillmurray.com/550/550"}
+            {src: "/static/jhicks-design/Banner.jpg"},
+            {src: "/static/jhicks-design/JsPython.png"},
+            {src: "/static/jhicks-design/Design.png"}
           ]
         }
 
